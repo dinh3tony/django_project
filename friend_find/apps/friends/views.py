ce0f850f09e09352cdfd8f27529f5cc43a565d3b
unknown <kqpham007@gmail.com>
2018-06-26 10:38:39 -0700

    ..

--- friend_find/apps/friends/views.py
+++ friend_find/apps/friends/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+
+def profile(request):
+	return render(request, "friends/profile.html")
